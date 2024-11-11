6838f9253eacb6271825bc4102f0a23614809cf0
Jarren San Jose <sanjose.jarren@gmail.com>
2024-11-10 16:23:07 -0800

    edit merch button, coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,11 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-gray-400 text-background gap-2 cursor-not-allowed opacity-50 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            role="button"
+            aria-disabled="true"
           >
-            Merch store
+            merch (coming soon)
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
